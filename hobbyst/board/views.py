b4f44ed52d98b2c4b5620ea8264b574008a25cc9
sxp <kt3585@gmail.com>
2024-11-14 03:08:13 +0900

    교재 254페이지까지 완성 [피드(board) 페이지 완성 및 피드의 프로필 이미지 스타일 변경]

--- hobbyst/board/views.py
+++ hobbyst/board/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
-
+from board.models import Post
 # Create your views here.
+
+
+
+
 
 def home(request):
     if not request.user.is_authenticated:
         return redirect('/account/login')
+    posts = Post.objects.all()
+    context = {'posts': posts}
 
-    return render(request, 'board/home.html')
+    return render(request, 'board/home.html', context)
+
