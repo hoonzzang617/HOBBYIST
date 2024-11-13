605ab3e4b087d352ce53bfa8f8dc26c74427dbab
sxp <kt3585@gmail.com>
2024-11-14 04:28:23 +0900

    교재 279페이지까지 완성 [글 및 댓글 작성 완료 - 글 이미지 사이즈 알맞게 수정 필요]

--- hobbyst/board/urls.py
+++ hobbyst/board/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from board.views import home
+from board.views import home, comment_add, comment_delete, post_add
 
 urlpatterns = [
     path('', home, name='home'),
+    path("comment_add/", comment_add),
+    path("comments/<int:comment_id>/delete/", comment_delete),
+    path("post_add/", post_add),
 ]
